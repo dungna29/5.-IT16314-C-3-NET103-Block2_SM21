a951153c162ab86aa50e2d93a849b3230a013b53
Dungna29 <dungna29@fpt.edu.vn>
2021-07-03 12:02:17 +0700

    1

---
+++ 5_IT16314_NET103_Block2_SM21/BAI_1_1_TongQuanWindowForm/TheLoai.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAI_1_1_TongQuanWindowForm
+{
+    class TheLoai
+    {
+        public int Id { get; set; }
+        public string MaTheLoai { get; set; }
+        public string TenTheLoai { get; set; }
+        public bool TrangThai { get; set; }
+        public bool statuEdit { get; set; }
+
+        public List<TheLoai> GetListTheLoais()
+        {
+            return new List<TheLoai>
+            {
+                new TheLoai{Id = 1,MaTheLoai = "TL1",TenTheLoai = "Small",TrangThai = true},
+                new TheLoai{Id = 2,MaTheLoai = "TL2",TenTheLoai = "Large",TrangThai = true},
+                new TheLoai{Id = 3,MaTheLoai = "TL3",TenTheLoai = "Full Size",TrangThai = false},
+            };
+        }
+    }
+}
